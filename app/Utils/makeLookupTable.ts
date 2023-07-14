1bbeb02320c57793e106574426757a3856daa2dc
Aoki <ger.vladislav2001@gmail.com>
2023-07-14 13:50:55 +0300

    Copy makeLookupTable from frontend

--- app/Utils/makeLookupTable.ts
+++ app/Utils/makeLookupTable.ts
@@ -1,20 +1,37 @@
 
+interface Wrapper<K, V> {
+	key: K
+	value: V
+}
+
+export type Lookup<
+	T,
+	K extends number | string | symbol,
+	U extends boolean | undefined,
+> = Record<K, Wrapper<K, U extends true ? T : T[]>>;
+
 export default function makeLookupTable<T, K extends number | string | symbol, U extends boolean | undefined>(
 	items: T[],
-	key: (item: T) => K,
+	getKey: (item: T) => K,
 	keyIsUnique: U,
 ) {
-	const lookup = {} as Record<K, T | T[]>;
+	const lookup: Partial<Lookup<T, K, U>> = {};
 
 	if(keyIsUnique)
+		// U is true
 		items.forEach(item => {
-			lookup[key(item)] = item;
+			const key = getKey(item);
+			(lookup as Lookup<T, K, true>)[key] = { key, value: item };
 		});
 	else
+		// U is false
 		items.forEach(item => {
-			lookup[key(item)] ??= []
-			;(lookup[key(item)] as T[]).push(item);
+			const key = getKey(item);
+			(lookup as Lookup<T, K, false>)[key] ??= { key, value: [] };
+
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			(lookup as Lookup<T, K, false>)[key]!.value.push(item);
 		});
 
-	return lookup as Record<K, U extends true ? T : T[]>;
+	return lookup as Lookup<T, K, U>;
 }
